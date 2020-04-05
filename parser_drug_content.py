5bfd72c7a863db3339b36040673b588da3be19e3
Vibe <s.dmitrievlol@yandex.ru>
2020-04-05 18:35:04 +0300

    async added

--- parser_drug_content.py
+++ parser_drug_content.py
@@ -1,7 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 import json
-from parser_links_gatherer import get_html, write_file
+from parser_links_gatherer import write_file
+import asyncio
+import aiohttp
 
 
 links_list = [
@@ -31,46 +33,50 @@ unused_blocks = {
         {'class':'ddc-reviewed-by'}
     ]
 }
+soups = []
 
-def drug_content(soup):
-    page = soup.find('div', attrs={'class':'contentBox'})#.get_text()
-    name = page.find('div', attrs={'class':'pronounce-title'}).h1.text
-
-    #Чистим ненужные блоки
-
+def clean_page(page):
+    '''Чистим ненужные блоки'''
     for key, value in unused_blocks.items():
         for val in value:
             try:
                 page.find(key, attrs=val).decompose()
             except:
-                print('Блок ')
-    '''
-    page.find('div', attrs={'class':'pronounce-title'}).decompose()
-    page.find('ul', attrs={'class':'nav-tabs'}).decompose()
-    page.find('p', attrs={'class':'ddc-reviewed-by'}).decompose()
-    page.find('div', attrs={'class':'navNext'}).decompose()
-    page.find('div', attrs={'class':'more-resources'}).decompose()
-    '''
+                print('Блок отсутствует')
 
+def drug_content(soup):
+    '''Возвращает готовый текст в html'''
+    page = soup.find('div', attrs={'class':'contentBox'})#.get_text()
+    name = page.find('div', attrs={'class':'pronounce-title'}).h1.text
+    clean_page(page)
     all_links = page.find_all('a')
     article = BeautifulSoup('', 'html.parser')
-    
     for link in all_links:
         #print(f'----{link.text}----')
         article.string = link.text
         link.replace_with(article)
+    return page
 
+async def get_html(url, session):
+    '''Запишем суп страницы в список супов'''
+    url = 'https://www.drugs.com{}'.format(url)
+    async with session.get(url) as response:
+        data = await response.read()
+        soups.append(BeautifulSoup(data, 'html.parser'))
 
-    write_file(str(page), fname='page_example.html')
+async def main():
+    tasks = []
+    async with aiohttp.ClientSession() as session:
+        for link in links_list:
+            task = asyncio.create_task(get_html(link, session))
+            tasks.append(task)
+        await asyncio.gather(*tasks)
 
-
-def page_prep(links_list):
-    url = 'https://www.drugs.com{}'.format(links_list[0])
-    return BeautifulSoup(get_html(url), 'html.parser')
-
-def main():
-    soup = page_prep(links_list)
+    '''
+    soup = page_prep(url)
     drug_content(soup)
-
+    '''
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
+    print(len(soups))
+    write_file(soups.pop().prettify(), fname='testasync.txt')
