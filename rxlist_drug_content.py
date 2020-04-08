5b359d734363fb205dfe0241cb80cc156e2257cf
Vibe <s.dmitrievlol@yandex.ru>
2020-04-08 17:51:32 +0300

    at work

--- rxlist_drug_content.py
+++ rxlist_drug_content.py
@@ -1,10 +1,13 @@
+# -*- coding: utf-8 -*-
 import requests
 from bs4 import BeautifulSoup
 from rxlist_collect_links import write_file, get_html
 import re
 from bs4 import element
 from foo import drugs #Тестируем
+import logging
 
+logging.basicConfig(filename='log.txt', level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s')
 
 FIELD_CONVERTS = {'INDICATIONS': 'Therapeutic indications', 'DESCRIPTION': None, 
                   'DOSAGE AND ADMINISTRATION': 'Dosage (Posology) and method of administration', 
@@ -33,31 +36,33 @@ def components_and_form_re(raw_components):
         if components.count('(') > components.count(')'):
             components += ')'
     except AttributeError as er:
-        print(f'Ошибка в регулярке: {er}')
+        #print(f'Ошибка в регулярке: {er}')
         return None
     return {'components': components, 'forms': forms}
       
 
-def combine_data(drug_description_contents):
+def combine_data(first_block_iter):
     '''
     Сбор данных для первого блока
     Объединяем данные из абзацев, где могут содержаться components и forms (в случае, если препарат комбинированный)
     '''
     result = {'components':  '', 'forms': ''}
-    for tag in drug_description_contents:
-        if tag.name == 'a':
-            break
-        else:
-            if tag.name == 'p':
-                print(tag.text)
-                comps_and_forms = components_and_form_re(tag.text)
-                # Сложим строки в случае, если препарат многокомпонентный.
-                if comps_and_forms:
-                    for key, val in comps_and_forms.items():
-                        if result[key]:
-                            result[key] += ', ' + val
-                        else:
-                            result[key] = val
+    try:
+        for tag in first_block_iter:
+            if tag.name == 'a':
+                break
+            else:
+                if tag.name == 'p':
+                    comps_and_forms = components_and_form_re(tag.text)
+                    # Сложим строки в случае, если препарат многокомпонентный.
+                    if comps_and_forms:
+                        for key, val in comps_and_forms.items():
+                            if result[key]:
+                                result[key] += ', ' + val
+                            else:
+                                result[key] = val
+    except Exception as e:
+        logging.debug(f'Ошибка в combine_data: {e}')
 
     return result
 
@@ -68,16 +73,14 @@ def link_cleaner(tag):
 def get_data_from_pgContent(pgContent):
     '''Собираем данные из одного блока. Возвращает словарь, ключ - имя поля таблицы описания препарата, значение - содержимое раздела'''
     result = {} # ключ - имя поля в таблице, значение - содержимое раздела
-    print('*-'*5)
     key = None
     for tag in pgContent.children:
         if isinstance(tag, element.Tag):
-            print(f'im here. tag name: {tag.name}, tag.text: {tag.text}')
             if tag.name == 'h3':
                 if tag.text in FIELD_CONVERTS:
                     tag_text = tag.text
                     key = FIELD_CONVERTS[tag_text] #Присваиваем имя поля в таблице как указано в ТЗ (в FIELD_CONVERTS это значения по ключам-разделам сайта)
-                    if key: # Проверим, нужен ли нам этот раздел сайта (Есть значение по ключу-разделу сайта)
+                    if key: # Проверим, нужен ли нам этот раздел сайта (Есть значение по ключу-разделу сайта в FIELD_CONVERTS)
                         result[key] = ''
                     #Значения разделов WARNINGS и PRECAUTIONS должны объединяться
                     if tag_text == 'WARNINGS' or tag_text == 'PRECAUTIONS':
@@ -94,51 +97,64 @@ def get_data_from_pgContent(pgContent):
                             if child.name == 'table':
                                 result[key] += str(child)
 
-    print(result)
     return result
 
+"""
 def get_all_headers(pgContent):
+    '''Служебная функция. Соберем все попадающиеся заголовки <h3>'''
     headers = set()
     for tag in pgContent.children:
         if isinstance(tag, element.Tag):
-            print(f'im here. tag name: {tag.name}, tag.text: {tag.text}')
             if tag.name == 'h3':
                 headers.add(tag.text)
     return headers
-
-HEADERS = set()
+"""
+#HEADERS = set()
 
 def get_data(soup):
-    '''Обработка страницы одного лекарства'''
+    '''Обработка страницы одного лекарства. Первый блок pgContent разбираем на drug name, components и forms. Остальные pgContent перебираем 
+       и отправляем в get_data_from_pgContent, откуда получаем словарь (этот словарь может иметь несколько ключей): 
+       ключ имя поля таблицы описания, значение - само описание. 
+       Расширяем результирующий словарь по препарату drug_data словариками из каждого pgContent
+    '''
 
     drug_data = {} # Сюда собираем все разделы в соответствующие поля (ключи словаря)
     pgContent_blocks = soup.find_all('div', attrs={'class': 'pgContent'})
-    print(pgContent_blocks[0].p)
-    drug_data['Drug name'] = pgContent_blocks[0].p.b.text.strip() #ПЛОХО!
-    
+    #drug_data['Drug name'] = pgContent_blocks[0].p.b.text.strip() #ПЛОХО!
     drug_data.update(combine_data(pgContent_blocks[0].children)) #Из первого блока берем components и forms
-    print(drug_data)
-    foo = True 
+
+
+    foo = True # Проверочная переменная для определения первого блока pgContent
     for pgContent in pgContent_blocks:
         # Пропустим первый блок
         if foo:
+            # Из первого блока возьмем первый абзац, в котором содержится имя препарата
+            for tag in pgContent.children:
+                if tag.name == 'p':
+                    #Есть страницы, где нет имени и компонентов/форм в разделах. В таком случае возьмем из заголовка страницы имя препарата
+                    try:
+                        drug_data['Drug name'] = tag.b.text.strip()
+                    except:
+                        drug_data['Drug name'] = soup.h1.text
+                        logging.debug(f'Имя {drug_data["Drug name"]} взято из резервного источника')
+                    break   
             foo = False
             continue
-        HEADERS.update(get_all_headers(pgContent))
-        
-        #get_data_from_pgContent(pgContent)
-        #break
+        data_from_pgContent = get_data_from_pgContent(pgContent)
+        drug_data.update(data_from_pgContent)
+    
+    return drug_data
+
 
 def main():
     for drug in drugs:
         soup = BeautifulSoup(get_html(drug), 'html.parser')
         get_data(soup)
-    write_file(list(HEADERS, fname='HEADERS.json'))
+    #write_file(list(HEADERS), fname='HEADERS.json')
 
 if __name__ == "__main__":
-
+    '''
     main()
     '''
     soup = BeautifulSoup(get_html(URL2), 'html.parser')
-    get_data(soup)
-    '''
+    write_file(get_data(soup), fname='test_drug_data.json')
