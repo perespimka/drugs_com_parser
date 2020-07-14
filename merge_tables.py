3ace40210228f72679d7407f7695c1517b755418
perespimka <s.dmitrievlol@yandex.ru>
2020-07-14 16:14:47 +0300

    merge added2

--- merge_tables.py
+++ merge_tables.py
@@ -67,50 +67,55 @@ def main():
             charset='utf8mb4'
         )
 
+        try:
+            for i in range(2, 33364):   #(2,33364)
+                lite_cursor = lite_connection.cursor()
+                try:
+                    query = 'SELECT * FROM ndrugs WHERE rowid=?'
+                    lite_cursor.execute(query, (i,))
+                    ndrugs_data = lite_cursor.fetchone()
 
-        for i in range(2, 33364):   #(2,33364)
-            lite_cursor = lite_connection.cursor()
 
-            try:
-                query = 'SELECT * FROM ndrugs WHERE rowid=?'
-                lite_cursor.execute(query, (i,))
-                ndrugs_data = lite_cursor.fetchone()
-
-
-                with mysql_connection.cursor() as my_cursor:
-                    query = 'SELECT id FROM medicines_new WHERE Drug_name=%s;'
-                    my_cursor.execute(query, (ndrugs_data['name'],))
-                    my_id = my_cursor.fetchone()
-                if my_id:
-                    medicines_new_fill(mysql_connection, ndrugs_data, my_id[0])
-
-                else:
-                    logger.debug(f'Insert {ndrugs_data["name"]}')
-                    print(f'Insert {ndrugs_data["name"]}')
                     with mysql_connection.cursor() as my_cursor:
-                        query = '''
-                                    INSERT INTO medicines_new (Drug_name,  Therapeutic_indications, Dosage_Posology, 
-                                    Contraindications, Special_warnings_and_precautions_for_use, Interaction,
-                                    Undesirable_effects, Overdose, description, ingredient_matches, list_of_subs) 
-                                    VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);
-                        '''
-                        my_cursor.execute(query, (
-                            ndrugs_data['name'], ndrugs_data['indications'] + ndrugs_data['what_is'], ndrugs_data['dosage'],
-                            ndrugs_data['contraindications'], ndrugs_data['how_should_i_use'] + ndrugs_data['uses_of_in_details'], 
-                            ndrugs_data['interactions'], ndrugs_data['side_effects'], ndrugs_data['overdose'], 
-                            ndrugs_data['description'], ndrugs_data['active_ingredient_matches'], ndrugs_data['list_of_substitutes']
-                        ))
-                        mysql_connection.commit()
-            except Exception as e:
-                print(f'ERROR: {e}')
-                logger.warning(f'ERROR: {e}')
-                print(traceback.format_exc())
+                        query = 'SELECT id FROM medicines_new WHERE Drug_name=%s;'
+                        my_cursor.execute(query, (ndrugs_data['name'],))
+                        my_id = my_cursor.fetchone()
+                    if my_id:
+                        medicines_new_fill(mysql_connection, ndrugs_data, my_id[0])
+
+                    else:
+                        logger.debug(f'Insert {ndrugs_data["name"]}')
+                        print(f'Insert {ndrugs_data["name"]}')
+                        with mysql_connection.cursor() as my_cursor:
+                            query = '''
+                                        INSERT INTO medicines_new (Drug_name,  Therapeutic_indications, Dosage_Posology, 
+                                        Contraindications, Special_warnings_and_precautions_for_use, Interaction,
+                                        Undesirable_effects, Overdose, description, ingredient_matches, list_of_subs) 
+                                        VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);
+                            '''
+                            my_cursor.execute(query, (
+                                ndrugs_data['name'], ndrugs_data['indications'] + ndrugs_data['what_is'], ndrugs_data['dosage'],
+                                ndrugs_data['contraindications'], ndrugs_data['how_should_i_use'] + ndrugs_data['uses_of_in_details'], 
+                                ndrugs_data['interactions'], ndrugs_data['side_effects'], ndrugs_data['overdose'], 
+                                ndrugs_data['description'], ndrugs_data['active_ingredient_matches'], ndrugs_data['list_of_substitutes']
+                            ))
+                            mysql_connection.commit()
+                except Exception as e:
+                    print(f'ERROR: {e}')
+                    logger.warning(f'ERROR: {e}')
+                    print(traceback.format_exc())
+                finally:
+                    lite_cursor.close()           
+        except Exception as e:
+            print(f'ERROR: {e}')
+            logger.warning(f'ERROR: {e}')
+            print(traceback.format_exc())
 
 
-            finally:
-                lite_cursor.close()
-                mysql_connection.close()
-                lite_connection.close()
+        finally:
+
+            mysql_connection.close()
+            lite_connection.close()
 
 
 
